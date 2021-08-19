45e7700320fa540d58d93d7112458e550cae7334
Prasanna0708 <78546913+Prasanna0708@users.noreply.github.com>
2021-08-19 21:59:05 +0530

    Add files via upload

---
+++ Screen_Recorder.py
@@ -0,0 +1,26 @@
+#import pacakges what we have installed now...opencv-python , pyautogui,numpy by using pip command
+import cv2
+import numpy as np
+import pyautogui
+import time
+
+screenShot = pyautogui.screenshot()
+#saving screenshot into image
+screenShot.save('image.png')
+#printing size of the screenshot
+#print(screenShot.size)
+imageResolution = screenShot.size
+
+fourCC = cv2.VideoWriter_fourcc(*"mp4v")
+frameRate = 30.0
+videoWriter = cv2.VideoWriter("video.mp4",fourCC,frameRate,imageResolution)
+
+for i in range(100):
+    screenShot = pyautogui.screenshot()
+    numpyImageArray = np.array(screenShot)
+    numpyImageArray = cv2.cvtColor(numpyImageArray,cv2.COLOR_BGR2RGB)
+    videoWriter.write(numpyImageArray)
+
+cv2.destroyAllWindows()
+videoWriter.release()
+
